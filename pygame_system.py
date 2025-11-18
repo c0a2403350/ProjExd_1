9c9b341cd219dcf500207553e945f63b9267e881
Impas <c0a2403350@edu.teu.ac.jp>
2025-11-18 15:49:00 +0900

    xとy座標のループの実装

--- pygame_system.py
+++ pygame_system.py
@@ -8,18 +8,22 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
     pg.display.set_caption("はじめてのPygame")
-    screen = pg.display.set_mode((800, 600))
+
+    w, h = 800, 600
+    screen = pg.display.set_mode((w, h))
     clock = pg.time.Clock()
     font = pg.font.Font(None, 80)
 
     enn = pg.Surface((20, 20))
-    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
+    r = 10
+    pg.draw.circle(enn, (255, 0, 0), (10, 10), r)
     
     enn.set_colorkey((0, 0, 0))
 
     tmr = 0
     x = 10
     y = 10
+
     while True:
         pressed_key = pg.key.get_pressed()
         if pressed_key[K_ESCAPE]: pg.quit()
@@ -27,6 +31,16 @@ def main():
         if pressed_key[K_LEFT]: x -= 5
         if pressed_key[K_UP]: y -= 5
         if pressed_key[K_DOWN]: y += 5
+        
+        if y > h + r:
+            y = 0
+        elif y < 0 - r:
+            y = h + r
+        
+        if x > w + r:
+            x = 0
+        elif x < 0 - r:
+            x = w + r
 
         for event in pg.event.get():
             if event.type == pg.QUIT: return
