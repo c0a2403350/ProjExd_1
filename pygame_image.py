7fc1f3b8892b3e87963c9237f749eef42375c37f
Impas <c0a2403350@edu.teu.ac.jp>
2025-11-18 16:40:40 +0900

    練習5の修正

--- pygame_image.py
+++ pygame_image.py
@@ -11,24 +11,30 @@ def main():
     
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    bird_img = pg.transform.scale(bg_img, (1600, 900))
+    bg_img = pg.transform.scale(bg_img, (1600, 900))
 
+    bg_img2 = pg.image.load("fig/pg_bg.jpg")
+    bg_img = pg.transform.scale(bg_img2, (1600, 900))
+    
     bird_img = pg.image.load("fig/3.png")
     bird_img = pg.transform.flip(bird_img, True, False)
     
     tmr = 0
-    x, y = 300, 200
+    x, y = 0, 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
-        screen.blit(bg_img, [0, 0])
-        screen.blit(bird_img, [x, y])
+        screen.blit(bg_img, [-x, y])
+        screen.blit(bird_img, [300, 200])
 
         pg.display.update()
         tmr += 1        
         clock.tick(200)
-
+        clock.tick(600)
+        
+        x = tmr
+        # screen.blit(bg_img2, [800 - x, 0])
 
 if __name__ == "__main__":
     pg.init()
