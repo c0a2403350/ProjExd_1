eb4439c584bd35697e7ad57cd63de2c458810158
Impas <c0a2403350@edu.teu.ac.jp>
2025-11-18 14:51:48 +0900

    初コミット

---
+++ pygame_system.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import pygame as pg
+from pygame.locals import *
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はじめてのPygame")
+    screen = pg.display.set_mode((800, 600))
+    clock = pg.time.Clock()
+    font = pg.font.Font(None, 80)
+
+    enn = pg.Surface((20, 20))
+    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
+    
+    enn.set_colorkey((0, 0, 0))
+
+    tmr = 0
+    x = 10
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+            pressed_key = pg.key.get_pressed()
+            if pressed_key[K_ESCAPE]: pg.quit()
+            if pressed_key[K_RIGHT]: x += 5
+            if pressed_key[K_LEFT]: x -= 5
+        
+        txt = font.render(str(tmr), True, (255, 255, 255))
+        screen.fill((50, 50, 50))
+        screen.blit(txt, [300, 200])
+        screen.blit(enn, [x, 400])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
